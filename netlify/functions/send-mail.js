caf3a5ee1c92547764e50b52355cb9f5280cf286
Yuta Okubo <nhnskk77@gmail.com>
2025-07-15 17:22:13 +0900

    コース追加

--- netlify/functions/send-mail.js
+++ netlify/functions/send-mail.js
@@ -14,26 +14,33 @@ exports.handler = async function (event, context) {
   });
 
   // 管理者（あなた）への通知メール
-  const adminMailOptions = {
-    from: process.env.GMAIL_USER,
-    to: "nhnskk77@gmail.com",
-    subject: `${data.date} ${data.time} ${data.course}`,
-    text: `お名前: ${data.name}
+const adminMailOptions = {
+  from: process.env.GMAIL_USER,
+  to: "nhnskk77@gmail.com",
+  subject: `${data.date} ${data.time} ${data.course}`,
+  text: `お名前: ${data.name}
 電話番号: ${data.phone}
 メール: ${data.email}
+ご希望セラピスト: ${data.therapist}
+ご希望コース: ${data.course}
+その他: ${data.others}
 ご希望日: ${data.date}
-ご希望時間: ${data.time}
-コース: ${data.course}
-決済方法: ${data.payment}
-ご要望・その他: ${data.notes}`
-  };
+待ち合わせ時間: ${data.time}
+待ち合わせ場所: ${data.meeting_place}
+最寄駅・指定場所について: ${data.meeting_detail}
+お支払い方法: ${data.payment}
+ご要望・その他: ${data.notes}
+割引申告: ${data.discount}`
+};
 
   // お客様への自動返信メール
-  const userMailOptions = {
-    from: process.env.GMAIL_USER,
-    to: data.email,
-    subject: "【蒼人】ご予約ありがとうございます",
-    text: `${data.name} 様
+const userMailOptions = {
+  from: process.env.GMAIL_USER,
+  to: data.email,
+  subject: "【LEAD】ご予約ありがとうございます",
+  text: `${data.name} 様
+
+※このメールは自動返信です。
 
 このたびはご予約ありがとうございます。
 受付を承りました。
@@ -41,16 +48,19 @@ exports.handler = async function (event, context) {
 事務局より改めてご連絡いたしますので、しばらくお待ちくださいませ。
 
 【送信内容確認】
+ご希望セラピスト: ${data.therapist}
+ご希望コース: ${data.course}
+その他: ${data.others}
 ご希望日: ${data.date}
-ご希望時間: ${data.time}
-コース: ${data.course}
-決済方法: ${data.payment}
+待ち合わせ時間: ${data.time}
+待ち合わせ場所: ${data.meeting_place}
+最寄駅・指定場所について: ${data.meeting_detail}
+お支払い方法: ${data.payment}
 ご要望・その他: ${data.notes}
-
-※このメールは自動返信です。
+割引申告: ${data.discount}
 
 事務局`
-  };
+};
 
   try {
     // 管理者へ送信
